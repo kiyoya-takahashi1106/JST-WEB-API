6da23900809bf0e5945af51fa362a779f057a0d1
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-12-24 17:55:59 +0900

    schemas-optional

--- schemas.py
+++ schemas.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 class ReservationRequest(BaseModel):
     uid: str
     organizer_name: str
     title: str
-    event_place: str
-    event_from_at: datetime
-    event_to_at: datetime
-    details: str
-    sns: str
-    sns_id: str
+    event_place: Optional[str] = None
+    event_from_at: Optional[datetime] = None
+    event_to_at: Optional[datetime] = None
+    details: Optional[str] = None
+    sns: Optional[str] = None
+    sns_id: Optional[str] = None
     display_place: str
     display_from_at: datetime
     display_to_at: datetime
