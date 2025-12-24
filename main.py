8df4f051e815f478dd491a615b7abd9edcaff906
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-12-24 19:22:47 +0900

    change cross-origin

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://jst-robot-project.web.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
