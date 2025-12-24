859ccd5fe19b1c82709dccdf19efcac4e183d1e1
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-12-24 17:56:36 +0900

    setting cross-origin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import firebase_init
 from firebase_admin import firestore
@@ -9,6 +10,15 @@ from schemas import ReservationRequest
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 db = firestore.client()
 
 
